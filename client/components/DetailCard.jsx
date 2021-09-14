a79f40f87155b7f89c4cea87c6ff7ac0e7e775aa
anniee825 <anniee825@gmail.com>
2021-09-14 10:23:54 -0400

    work on detailcard

--- client/components/DetailCard.jsx
+++ client/components/DetailCard.jsx
@@ -2,7 +2,46 @@ import React, { useState } from "react";
 
 const DetailCard = () => {
 
-  // return ()
+  // deconstruct props to pass down questions in different sections?
+  return (
+    <>
+      <div id = "DetailsCard">
+        <div id = "Identifiers">
+          <p>Company: </p>
+          <p>Team: </p>
+          <p>Role: </p>
+        </div>
+
+        <div class = "Questions">
+          <ol>
+            {/* //loop through array of behavior questions passed down and render a list item for each */}
+            1.
+            2.
+            3.
+          </ol>
+        </div>
+        
+        <div class = "Questions">
+          <ol>
+            {/* //loop through array of algo questions passed down and render a list item for each */}
+            1.
+            2.
+            3.
+          </ol>
+        </div>
+
+        <div class = "Questions">
+          <ol>
+            {/* //loop through array of system questions passed down and render a list item or each */}
+            1.
+            2.
+            3.
+          </ol>
+        </div>
+        
+      </div>
+    </>
+  )
 }
 
 export default DetailCard;
