95134f2035fd0c2d034e5332542303f27fcd90c0
anniee825 <anniee825@gmail.com>
2021-09-14 12:07:15 -0400

    work on reducer, about to pull server in

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FeedContainer from './containers/FeedContainer.jsx'
-import MainContainer from './containers/MainContainer.jsx'
+import UserContainer from './containers/UserContainer.jsx'
 
 
 function App () {
@@ -10,12 +10,11 @@ function App () {
   return (
     <Router>
       <Switch>
-        <Route exact path="/feed" component = {NavBar} component={FeedContainer}/>
+        <Route exact path="/feed" component={FeedContainer}/>
         <Route exact path="/personalPage" component={UserContainer} />
       </Switch>
     </Router>
   )   
 }
-
 
 export default App;
