d5d16126b8a1dbd5b7b54f14a66016d8a35cc5d4
anniee825 <anniee825@gmail.com>
2021-09-14 11:24:42 -0400

    added router, finished feedcard and navbar

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FeedContainer from './containers/FeedContainer.jsx'
+import MainContainer from './containers/MainContainer.jsx'
+
 
 function App () {
 
-  // return () 
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/feed" component = {NavBar} component={FeedContainer}/>
+        <Route exact path="/personalPage" component={UserContainer} />
+      </Switch>
+    </Router>
+  )   
 }
 
 
--- client/components/FeedCard.jsx
+++ client/components/FeedCard.jsx
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 
 const FeedCard = () => {
-  
+  //take from store
+  // display all details, maybe load a bunch at a time but not all ?
+  // logic goes here, 
   return (
 
+    <>
+      <p>Date:</p>
+      <p>Company:</p>
+      <p>Team: </p>
+
+      <h5>Algo problems</h5>
+      {/* loop through algo problems and render a list item for each*/}
+      
+      <h5>Behavior questions</h5> 
+      {/* loop through behavior problems and render a list item for each*/}
+
+      <h5>System design questions</h5> 
+      {/* loop through system design problems and render a list item for each*/}
+      
+    </>
   )
 }
 
