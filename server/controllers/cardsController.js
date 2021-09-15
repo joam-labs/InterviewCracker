72521209889e1fe3783446ec0f789b12ae9edac4
Alex Hord <myspacealexr1der@gmail.com>
2021-09-15 17:22:31 +0300

    sorted

--- server/controllers/cardsController.js
+++ server/controllers/cardsController.js
@@ -2,33 +2,12 @@ const db = require('../db/model');
 
 const cardsController = {}; 
 
-cardsController.addCard = async (req, res, next) => {
-  
-  try {
-    const { firstName, lastName, username, password } = req.body;
-    const params = [ firstName, lastName, username, password ];
-    //const query = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id';
-    const query = ``
-    const { rows } = await db.query(query, params);
-    // console.log('rows =', rows);
-    res.locals.username = rows[0].username;
-    res.locals.id = rows[0].id; // To return to client the username that was added
-
-    return next();
-  } catch (err) {
-    return next({
-      status: 500,
-      message:
-        'Could not add Card',
-    });
-  }
-};
 cardsController.getAllCards =async (req, res, next) => {
   
     try {
      //const {id} = req.cookies();
            //const query = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id';
-      const query = `SELECT company_name, Role_details, Team, interviewstage, date, behavioral_questions, algo_questions, system_design, followupstatus, acceptstatus, rejectstatus FROM company JOIN cards ON company.id=cards.company_id WHERE User_id=1 LIMIT 10`;
+      const query = `SELECT company_name, Role_details, Team, interviewstage, date, behavioral_questions, algo_questions, system_design, followupstatus, acceptstatus, rejectstatus FROM company JOIN cards ON company.id=cards.company_id WHERE User_id=1 ORDER BY date DESC LIMIT 10`;
      // selecting 20 cards limit 20 and in decreasing order by time
      const { rows } = await db.query(query);
 
